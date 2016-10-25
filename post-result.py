a7e7bc37beb53463201077b7f6bb50ab3264b74b
pengt2 <Tian.Peng@emc.com>
2016-10-25 10:02:10 +0800

    remove logs in comments

--- post-result.py
+++ post-result.py
@@ -15,7 +15,8 @@ python post-result.py \
 --public_jenkins_url http://147.178.202.18/
 '''
 
-with open('${HOME}/.ghtoken', 'r') as file:
+#with open('${HOME}/.ghtoken', 'r') as file:
+with open('/home/onrack/.ghtoken', 'r') as file:
     TOKEN = file.read().strip('\n')
 
 HEADERS = {'Authorization': 'token %s' % TOKEN}
@@ -70,109 +71,6 @@ def get_build_data(build_url):
         data = r.json()
         return data
 
-def get_test_report(build_url):
-    '''
-    get the test report of a build
-    :param build_url: the url of a build in jenkins
-    :return: json data of test report of the build if succeed to get the json data
-             None if failed to get the json data
-    '''
-    r = requests.get(build_url + "/testReport/api/json")
-    if is_error_response(r):
-        print "Failed to get testReport of {0}".format(build_url)
-        print r.text
-        print r.status_code
-        return None
-    else:
-        data = r.json()
-        return data     
-
-def generate_failure_report(build_url, build_name):
-    '''
-    get the test report and generate a report which contains the error log
-    :param build_url: the url of a build in jenkins
-    :param build_name: build name consist of job name and build number
-    :return: failure report if succeed to get the json data of test report 
-             None if failed to get the json data of test report
-    '''
-    data = get_test_report(build_url)
-    failure_report = "\n"
-    failure_report += "BUILD " + build_name + "  Error Logs:\n\n"
-    if data:
-        failCount = int(data.get('failCount'))
-        if failCount > 0:
-            for suite in data['suites']:
-                for case in suite['cases']:
-                    if case['errorDetails']:
-                        name = case['name']
-                        errorDetails = case['errorDetails']
-                        errorStackTrace = case['errorStackTrace']
-                        name = "Test Name: " + name + "\n"
-                        details = "Error Details: " + errorDetails + "\n"
-                        stack = "Stack Trace: " + errorStackTrace + "\n"
-                        failure_report += name + details + stack + "\n"
-        return failure_report
-    else:
-        return None
-
-def get_sub_builds(build_url, depth = 1):
-    '''
-    get sub builds of a build
-    :param build_url: the url of a build in jenkins
-    :param depth: the depth of the nested sub build
-    :return: a list of string which contains the result , build number of the sub builds
-    '''
-    build_data = get_build_data(build_url)
-    outputs = []
-    if build_data:
-        if 'subBuilds' in build_data:
-            '''
-            get sub builds results
-            '''
-            for subBuild in build_data['subBuilds']:
-                output = ""
-                sub_build_name = subBuild['jobName'] + "  #" + str(subBuild['buildNumber'])
-                sub_build_result = subBuild['result']
-                sub_build_url = JENKINS_URL + "/" + subBuild['url']
-                public_sub_build_url = sub_build_url.replace(JENKINS_URL, PUBLIC_JENKINS_URL)
-                for x in range(depth):
-                    output += "   "
-                output += "- ** BUILD [" + sub_build_name + "](" + public_sub_build_url + "): " + sub_build_result + "\n"
-
-                sub_outputs = get_sub_builds(sub_build_url, depth = depth+1)
-                if len(sub_outputs) >0 :
-                    for sub_output in sub_outputs:
-                        output += sub_output
-                outputs.append(output)
-                if sub_build_result != "SUCCESS":
-                    failure_report = generate_failure_report(sub_build_url, sub_build_name)
-                    FAIL_REPORTS.append(failure_report)
-
-        if 'actions' in build_data:
-            for action in build_data['actions']:
-                if 'triggeredBuilds' in action:
-                    '''
-                    get triggered builds results
-                    '''
-                    for subBuild in action['triggeredBuilds']:
-                        output = "\n"
-                        sub_build_name = subBuild['fullDisplayName']
-                        sub_build_result = subBuild['result']
-                        sub_build_url = subBuild['url']
-                        public_sub_build_url = sub_build_url.replace(JENKINS_URL, PUBLIC_JENKINS_URL)
-                        for x in range(depth):
-                            output += "   "
-                        output += "- ** BUILD [" + sub_build_name + "](" + public_sub_build_url + "): " + sub_build_result + "\n"
-                        sub_outputs = get_sub_builds(sub_build_url, depth = depth+1)
-                        if len(sub_outputs) > 0:
-                            for sub_output in sub_outputs:
-                                output += sub_output
-                        outputs.append(output)
-                        if sub_build_result != "SUCCESS":
-                            failure_report = generate_failure_report(sub_build_url, sub_build_name)
-                            FAIL_REPORTS.append(failure_report)
-    return outputs
-
 def is_error_response(res):
     """
     check the status code of http response
@@ -218,36 +116,13 @@ def main():
             build_name = build_data['fullDisplayName']
             build_result = build_data['result']
             OUTPUT +=  "BUILD [" + build_name + "](" + public_build_url + ") : " + build_result + "\n"
-
-            sub_build_outputs = get_sub_builds(BUILD_URL)
-            for sub_output in sub_build_outputs:
-                OUTPUT += sub_output
-
-            if build_result != "SUCCESS":
-                failure_report = generate_failure_report(BUILD_URL, build_name)
-                FAIL_REPORTS.append(failure_report)
-
-            if len(FAIL_REPORTS) > 0:
-                for fail_report in FAIL_REPORTS:
-                    OUTPUT += fail_report
-        else:
-            build_name = job_name + " #" + build_number   
-            failure_report = generate_failure_report(BUILD_URL, build_name)
-            FAIL_REPORTS.append(failure_report)
-
-            if len(FAIL_REPORTS) > 0:
-                for fail_report in FAIL_REPORTS:
-                    OUTPUT +=  "BUILD [" + build_name + "](" + public_build_url + ") :\n"
-                    OUTPUT += fail_report
-            else:
-                OUTPUT += "*** BUILD [" + build_name + "](" + public_build_url + ") ***\n"
     except Exception as e:
         print e
         sys.exit(1)
     finally:
         if OUTPUT == "":
             build_name = job_name + " #" + build_number
-            OUTPUT += "*** BUILD [" + build_name + "](" + public_build_url + ") ***\n"
+            OUTPUT += "BUILD [" + build_name + "](" + public_build_url + ") \n"
         post_comments_to_github(OUTPUT, GITHUB_PR_URL, HEADERS)
 
 if __name__ == "__main__":
@@ -258,5 +133,4 @@ if __name__ == "__main__":
     BUILD_URL = parsed_args.build_url
     BUILD_NAME = parsed_args.build_name
     PUBLIC_JENKINS_URL = parsed_args.public_jenkins_url
-
     main()
